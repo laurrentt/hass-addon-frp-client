015c0571d7b7d2486f8fb58ccfc09c6eaa200221
Laurent Jalbert-Simard <laurent_js@hotmail.com>
2025-11-24 23:48:15 -0500

    feat(frp-client): Simplify configuration to use raw frpc config
    - Replace individual configuration options with single frpcConfig option
    - Remove serverAddr, serverPort, authToken, customDomain, homeAssistantPort, extraPorts, and configurationSuffix options
    - Simplify run.sh to write user-provided frpc configuration directly instead of using sed replacements
    - Update config.yaml schema to accept raw frpc configuration string
    - Include example frpc configuration with letsencrypt validation and Home Assistant sections
    - Reduce complexity and improve flexibility by allowing users to provide complete frpc configuration

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -8,37 +8,8 @@ function stop_frpc() {
     kill -15 "${WAIT_PIDS[@]}"
 }
 
-bashio::log.info "Copying configuration."
-cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
-sed -i "s/server_addr = \"your_server_addr\"/server_addr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
-sed -i "s/server_port = 7000/server_port = $(bashio::config 'serverPort')/" $CONFIG_PATH
-sed -i "s/token = \"123456789\"/token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
-sed -i "s/custom_domains = \"custom_domain\"/custom_domains = \"$(bashio::config 'customDomain')\"/" $CONFIG_PATH
-sed -i "s/local_port = \"8123\"/local_port = \"$(bashio::config 'homeAssistantPort')\"/" $CONFIG_PATH
-sed -i "s/remote_port = \"443\"/remote_port = \"$(bashio::config 'remotePort')\"/" $CONFIG_PATH
-sed -i "s/\[letsencryptvalidation\]/\[letsencryptvalidation$(bashio::config 'configurationSuffix')\]/" $CONFIG_PATH
-sed -i "s/\[homeassistant\]/\[homeassistant$(bashio::config 'configurationSuffix')\]/" $CONFIG_PATH
-
-# Add extra ports if configured
-if bashio::config.has_value 'extraPorts'; then
-    bashio::log.info "Adding extra ports configuration"
-    for port in $(bashio::config 'extraPorts|keys'); do
-        PORT_NUM=$(bashio::config "extraPorts[${port}].port")
-        PORT_TYPE=$(bashio::config "extraPorts[${port}].type")
-        SUFFIX=$(bashio::config 'configurationSuffix')
-        
-        bashio::log.info "Adding port ${PORT_NUM} with type ${PORT_TYPE}"
-        
-        cat >> $CONFIG_PATH << EOF
-
-[homeassistant_extra_port_${PORT_NUM}${SUFFIX}]
-type = "${PORT_TYPE}"
-local_port = "${PORT_NUM}"
-remote_port = "${PORT_NUM}"
-custom_domains = "$(bashio::config 'customDomain')"
-EOF
-    done
-fi
+bashio::log.info "Writing configuration from user input."
+bashio::config 'frpcConfig' > $CONFIG_PATH
 
 bashio::log.info "Starting frp client"
 
